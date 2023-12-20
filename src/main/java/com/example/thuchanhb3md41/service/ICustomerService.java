519ce56591b512866e3defe910e33b95182f3645
TCTrung <trungtrinh1325@gmail.com>
2023-12-20 15:01:44 +0700

    success

---
+++ src/main/java/com/example/thuchanhb3md41/service/ICustomerService.java
@@ -0,0 +1,18 @@
+package com.example.thuchanhb3md41.service;
+
+import com.example.thuchanhb3md41.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+
+}
